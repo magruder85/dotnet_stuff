0fb02f53b1f5a83256951068e6748d603cc4b07e
Micah Magruder <Micah.Magruder@Cerner.com>
2020-03-15 08:14:46 -0400

    fun times with string interpolation and argument validation

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -6,7 +6,16 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub Actions!");
+            // Validate the args
+            if(args.Length > 0 ) {
+                // broke - composite formatting
+                // Console.WriteLine("Hello " + args[0] + "!");
+                // woke - string interpolation
+                Console.WriteLine($"Hello {args[0]}!");               
+            } else {
+                Console.WriteLine("This program would be a lot cooler with at least one argument.");
+            }
+
         }
     }
 }
