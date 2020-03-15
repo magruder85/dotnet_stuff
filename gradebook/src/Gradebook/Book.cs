804a58d7131fbb3bbafeeb2bd787c73bae9fda7f
Micah Magruder <Micah.Magruder@Cerner.com>
2020-03-15 14:14:40 -0400

    adding the Book class

---
+++ gradebook/src/Gradebook/Book.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Book 
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<double> grades;
+        private string name; 
+
+    }
+}
--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -7,21 +7,22 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            
+            var book = new Book("Micah's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+
+            grades.Add(56.1);
       
-           var result = 0.0;
-           foreach(double number in grades){
-               result += number;
-           }
-
-           result /= grades.Count;
-           System.Console.WriteLine($"The average grade is {result}.");
-
-            if(args.Length > 0 ) {
-                Console.WriteLine($"Hello {args[0]}!");
-            } else {
-                Console.WriteLine("This program would be a lot cooler with at least one argument.");
+            var result = 0.0;
+            foreach(double number in grades){
+                result += number;
             }
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result}.");
 
         }
     }
