2a7db1b8d07dd41cd4f1f22ade006df183b75b54
Micah Magruder <Micah.Magruder@Cerner.com>
2020-03-15 09:36:55 -0400

    adding some numbers

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,12 +7,18 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            // Validate the args
+           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+      
+           var result = 0.0;
+           foreach(double number in grades){
+               result += number;
+           }
+
+           result /= grades.Count;
+           System.Console.WriteLine($"The average grade is {result}.");
+
             if(args.Length > 0 ) {
-                // broke - composite formatting
-                // Console.WriteLine("Hello " + args[0] + "!");
-                // woke - string interpolation
-                Console.WriteLine($"Hello {args[0]}!");               
+                Console.WriteLine($"Hello {args[0]}!");
             } else {
                 Console.WriteLine("This program would be a lot cooler with at least one argument.");
             }
